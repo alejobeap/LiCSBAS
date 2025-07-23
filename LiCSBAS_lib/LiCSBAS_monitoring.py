9068e1a6676dac6924179f8c5e559a6e29f35da1
Pedro Alejandro Espín <65787135+alejobeap@users.noreply.github.com>
2025-07-23 16:39:21 +0100

    Update LiCSBAS_monitoring.py

--- LiCSBAS_lib/LiCSBAS_monitoring.py
+++ LiCSBAS_lib/LiCSBAS_monitoring.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+"""
+========
+Overview
+========
+Python3 library of monitoring functions for LiCSBAS.
+
+=========
+Changelog
+=========
+20250723 Pedro Espin-Bedon, Uni of Leeds
+ - Original implementation
+
+"""
 
